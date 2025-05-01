5884e0ea662f7720fc305273cd89d8463671d746
sakshi-jadhav-24 <sakshijadhav7976@gmail.com>
2025-05-01 18:41:36 +0530

    Update model.py

--- src/model.py
+++ src/model.py
@@ -2,39 +2,27 @@ import torch
 import torch.nn as nn
 
 class TrafficCNN(nn.Module):
-    def _init_(self):
-        super(TrafficCNN, self)._init_()
-        self.conv1 = nn.Conv2d(3, 32, 3, padding=1)
-        self.conv2 = nn.Conv2d(32, 64, 3, padding=1)
-        self.pool1 = nn.MaxPool2d(2, 2)
+    def __init__(self):
+        super(TrafficCNN, self).__init__()
+        self.net = nn.Sequential(
+            nn.Conv2d(3, 32, 3, padding=1), nn.ReLU(),
+            nn.Conv2d(32, 64, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2, 2),
 
-        self.conv3 = nn.Conv2d(64, 128, 3, padding=1)
-        self.conv4 = nn.Conv2d(128, 256, 3, padding=1)
-        self.pool2 = nn.MaxPool2d(2, 2)
+            nn.Conv2d(64, 128, 3, padding=1), nn.ReLU(),
+            nn.Conv2d(128, 256, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2, 2),
 
-        self.conv5 = nn.Conv2d(256, 512, 3, padding=1)
-        self.pool3 = nn.MaxPool2d(2, 2)
+            nn.Conv2d(256, 512, 3, padding=1), nn.ReLU(),
+            nn.MaxPool2d(2, 2),
 
-        self.fc1 = nn.Linear(512 * 4 * 4, 1024)
-        self.fc2 = nn.Linear(1024, 43)
-        self.relu = nn.ReLU()
+            nn.Flatten(),
+            nn.Linear(512 * 4 * 4, 1024), nn.ReLU(),
+            nn.Linear(1024, 43)
+        )
 
     def forward(self, x):
-        x = self.relu(self.conv1(x))
-        x = self.relu(self.conv2(x))
-        x = self.pool1(x)
-
-        x = self.relu(self.conv3(x))
-        x = self.relu(self.conv4(x))
-        x = self.pool2(x)
-
-        x = self.relu(self.conv5(x))
-        x = self.pool3(x)
-
-        x = x.view(x.size(0), -1)
-        x = self.relu(self.fc1(x))
-        x = self.fc2(x)
-        return x
+        return self.net(x)
 
 def load_model(model_path):
     model = TrafficCNN()
